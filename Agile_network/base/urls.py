3688be87cc51847a3854da4565ce8ae261391ded
Michael Mwangi <michaelmichire2016@gmail.com>
2024-11-21 16:02:46 +0300

    Integration with front end

--- Agile_network/base/urls.py
+++ Agile_network/base/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.LoginPage, name="login"),
+    path('logout/', views.LogoutUser, name="logout"),
+    path('register/', views.RegisterPage, name="register"),
+
     path('', views.home, name="home"),
-    path('room/', views.room, name="room"),
+    path('room/<str:pk>', views.room, name="room"),
+    path('profile/<str:pk>/', views.UserProfile, name='user-profile'),
+
+    path('create-room/', views.CreateRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.UpdateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.DeleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.DeleteMessage, name="delete-message"),
+
+    path('update-user/', views.UpdateUser, name="update-user"),
+    path('topics/', views.TopicsPage, name="topics"),
 ]
